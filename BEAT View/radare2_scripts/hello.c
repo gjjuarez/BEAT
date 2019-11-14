0b7558db843d1937cc3635f568ea29cd99bffa81
Manuel Munoz <mmunoz38@miners.utep.edu>
2019-11-13 23:01:41 -0500

    Added struct to hello file

--- BEAT View/radare2_scripts/hello.c
+++ BEAT View/radare2_scripts/hello.c
@@ -6,9 +6,15 @@ void addition(int addVar) {
 }
 int otherGlobal = 6;
 
+struct S1 {
+    int x;
+    int z;
+};
+
 int main(){
 	int myVar = 5;
 	printf("hello world");
 	addition(myVar);
+	struct S1 myStruct = {12, 3};
 	return 0;
 }

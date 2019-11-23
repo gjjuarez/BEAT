16bf006f0e374b5cd6c240b7c4540c49a8b36c09
Manuel Munoz <mmunoz38@miners.utep.edu>
2019-11-23 17:18:32 -0500

    Refactored if statements to be more concise. Updated functions filter.

--- BEAT View/radare2_scripts/filter.py
+++ BEAT View/radare2_scripts/filter.py
@@ -7,11 +7,9 @@ def filter_string(value, section, address):
     if strings_poi is None:
         return True
     for s in strings_poi:
-        if s["String Name"] in value:
-            return True
-        elif s["Section"] in section:
-            return True
-        elif s["Address"] == address:
+        if s["String Name"] in value\
+                or s["Section"] in section\
+                or s["Address"] == address:
             return True
     return False
 
@@ -20,23 +18,44 @@ def filter_var(name, value, vartype, size, address):
     if vars_poi is None:
         return True
     for v in vars_poi:
-        if v["Variable Name"] in name:
-            return True
-        elif v["Variable Value"] in value:
-            return True
-        elif v["Call From Address"] == address:
-            return True
-        elif v["Variable Type"] in vartype:
-            return True
-        elif v["Variable Size"] == size:
+        if v["Variable Name"] in name\
+                or v["Variable Value"] in value\
+                or v["Call From Address"] == address\
+                or v["Variable Type"] in vartype\
+                or v["Variable Size"] == size:
             return True
     return False
 
-def filter_function(name, param_order=[], param_values=[], ret_value="0", call_from="", destination=""):
+def filter_function(name, ret_type, ret_value, address, dest_address, call_from="", param_order=[], param_values=[]):
     functions = data_manager.get_pois_from_plugin_and_type("function")
     if functions is None:
         return True
     for func in functions:
-        if func["Function Name"] in name:
+        if func["Function Name"] in name\
+                or func["Return Type"] in ret_type\
+                or func["Return Value"] == ret_value\
+                or func["Address"] == address\
+                or func["Call From"] in call_from\
+                or func["Destination Address"] == dest_address:
+            return True
+        inOrder = True
+        # check if plugin parameters match gien parameters
+        for func_param, plugin_param in param_order, func["Parameter Order"]:
+            # types don't need to match exactly
+            # Ex: int can be int32_t or int16_t
+            if func_param not in plugin_param:
+                inOrder = False
+                break
+        if inOrder:
+            return True
+        valueInOrder = False
+        # check if plugin param values match given parameters
+        for func_type, plugin_type in param_values, func["Parameter Values"]:
+            # values need to match exactly
+            # Ex: 8 shouldn't match 81
+            if func_type != plugin_type:
+                valueInOrder = False
+                break
+        if valueInOrder:
             return True
     return False
